318e22fc66d8e3b36ff6759113e5bfa2a0174a48
hp910219 <792089975@qq.com>
2019-06-11 16:48:44 +0800

    连接二

--- JYWeb.py
+++ JYWeb.py
@@ -47,10 +47,9 @@ def tcm_api():
     if ports is None:
         error_message = 'No ports found in config.conf'
     elif api_service not in ports:
-        error_message = u'暂无此服务：%s. 目前服务有：%s' % (api_service, ports.keys())
+        error_message = u'暂无此服务：%s. 目前服务有：%s\n' % (api_service, ports.keys())
     else:
         api_url = endpoint + ':' + ports[api_service] + url
-        send_msg_by_dd(api_url)
         request_params = {'json': data} if method != 'GET' else {'params': data}
         headers = {'Content-Type': 'application/json'}
         if auth:
@@ -59,22 +58,18 @@ def tcm_api():
         try:
             response = requests.request(method, api_url, **request_params)
         except Exception, e:
-            error_message = str(e)
+            error_message = '%s\n' % str(e)
             response = None
         if response is not None:
             if response.status_code != 200:
-                error_message = "%s %s %d %s" % (api_url, "POST", response.status_code, response.text)
+                error_message = "%s %s %d %s\n" % (api_url, "POST", response.status_code, response.text)
             else:
                 response_data = response.json()
                 status = response_data.get('status')
-                if success_status is None:
-                    return jsonify(response_data)
-                if success_status == str(response_data['status']):
-                    return jsonify(response_data)
         error_message += u'【api】：%s\n' % api_url
     error_message += u'【访问地址】：%s\n' % request.url
     error_message += u'【请求方式】：%s\n' % method
-    error_message += u'【请求服务】：%s:%s\n' % (api_service, endpoint)
+    error_message += u'【请求服务】：%s\n' % api_service
 
     error_message += u'【请求数据】：%s\n' % json.dumps(data)
     if status is not None:
@@ -84,7 +79,10 @@ def tcm_api():
     error_message += u'【返回数据】：%s\n' % json.dumps(response_data)
     # if self.is_print:
     #     print error_message
-    send_msg_by_dd(error_message, env=env)
+    try:
+        send_msg_by_dd(error_message, env=env)
+    except:
+        print(error_message)
     return jsonify(response_data)
 
 
