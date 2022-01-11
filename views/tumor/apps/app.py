4e7a5c00aacb5a4f6c3056dae26f6a9ae5dce6b0
hp910219 <hp910219@126.com>
2022-01-11 14:42:04 +0800

    中医----去重，

--- views/tumor/apps/app.py
+++ views/tumor/apps/app.py
@@ -602,6 +602,22 @@ def sort_merge_snv_cnv(rq, r_path, output, result_dir, t):
     return cmd, [dir1, dir2]
 
 
+def sort_cor(rq, r_path, output, result_dir, t):
+    input_file1 = sort_app_file('input1', 'input_file1', result_dir, t)
+    input_file2 = sort_app_file('input2', 'input_file2', result_dir, t)
+    dir1 = os.path.dirname(input_file1)
+    dir2 = os.path.dirname(input_file2)
+    cmd = 'Rscript %s %s %s %s %s %s' % (
+        r_path,
+        input_file1,
+        input_file2,
+        rq.get('use'),
+        rq.get('method'),
+        output,
+    )
+    return cmd, [dir1, dir2]
+
+
 def sort_combination_ggplot(rq, r_path, output, result_dir, t):
     input_file1 = sort_app_file('input1', 'input_file1', result_dir, t)
     dir1 = os.path.dirname(input_file1)
